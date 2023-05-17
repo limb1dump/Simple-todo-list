1e15000e0306e5f0279f06410042385f71773e99
limb1dump <skywalkervaider@gmail.com>
2023-05-17 12:03:57 +0300

    Update App.jsx
    added controlled input

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [newItem, setNewItem] = useState('')
 
   return (
     <>
       <form className='new-item-form'>
         <div className='form-row'>
           <label htmlFor='item'>New Item</label>
-          <input type='text' id='item' />
+          <input
+            type='text'
+            id='item'
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+          />
         </div>
         <button className='btn'>Add</button>
       </form>
+
       <h1 className='header'>Todo List</h1>
+
       <ul className='list'>
         <li>
           <label>
