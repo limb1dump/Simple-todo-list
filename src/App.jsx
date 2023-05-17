83489309cc7c270024cc54417a71f86cb6b97cda
limb1dump <skywalkervaider@gmail.com>
2023-05-17 11:16:41 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>New Item</label>
+          <input type='text' id='item' />
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+    </>
+  )
+}
+
+export default App
