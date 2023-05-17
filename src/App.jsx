e192667efee8a96f271bfac20b5adf2e3ddb6d95
limb1dump <skywalkervaider@gmail.com>
2023-05-17 15:00:25 +0300

    Interactivity patch
    1. Added ability to add custom todos.
    2. Added functional checkboxes
    3. Added ability to delete todos

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,39 @@ import './App.css'
 function App() {
 
   const [newItem, setNewItem] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setTodos(currentTodos => {
+      return [...currentTodos, { id: crypto.randomUUID(), title: newItem, completed: false }]
+    })
+
+    setNewItem('')
+  }
+
+  const toggleTodo = (id, check) => {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: check }
+        }
+        return todo;
+      })
+    })
+
+  }
+
+  const deleteTodo = (id) => {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+  }
 
   return (
     <>
-      <form className='new-item-form'>
+      <form onSubmit={handleSubmit} className='new-item-form'>
         <div className='form-row'>
           <label htmlFor='item'>New Item</label>
           <input
@@ -23,13 +52,28 @@ function App() {
       <h1 className='header'>Todo List</h1>
 
       <ul className='list'>
-        <li>
-          <label>
-            <input type='checkbox' />
-            Item 1
-          </label>
-          <button className='btn btn-danger'>Delete</button>
-        </li>
+        {todos.length === 0 && 'No todos'}
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type='checkbox'
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button
+                className='btn btn-danger'
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          )
+        })}
+
       </ul>
     </>
   )
